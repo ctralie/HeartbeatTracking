7579b752cc61f0c99cfaf4ae9e87554d8589fec6
Chris Tralie <chris.tralie@gmail.com>
2016-06-04 09:20:31 -0400

    Adding dlib landmark reading files

---
+++ getKeypointsDlib.m
@@ -0,0 +1,9 @@
+function [X] = getKeypointsDlib(filename)
+    dpath = 'dlib-18.18/examples';
+    command = sprintf('%s/build/jh_face_landmark_ex %s/faces/shape_predictor_68_face_landmarks.dat %s temp.txt', dpath, dpath, filename);
+    system(command);
+    fin = fopen('temp.txt');
+    X = cell2mat(textscan(fin, '(%f, %f)'));
+    fclose(fin);
+end
+
